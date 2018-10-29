bd8b829bef5e5e3c40560cce8de2bdbb642b52fa
Pravin Khabile <khabilepravin@gmail.com>
2018-10-29 21:43:28 +1100

    Skeleton for parser component

---
+++ server/parser/IParser.cs
@@ -0,0 +1,10 @@
+﻿using dataModel;
+using System.Collections.Generic;
+
+namespace mgparser
+{
+    interface IParser
+    {
+        IEnumerable<ParsedText> Parse(MediaText mediaText);
+    }
+}
---
+++ server/parser/Parser.cs
@@ -0,0 +1,119 @@
+﻿using dataModel;
+using System.Collections.Generic;
+
+namespace mgparser
+{
+    public class Parser : IParser
+    {
+        public static readonly string RGXSQUAREBRACKET = @"\[[^\]]*\]";
+        public static readonly string RGXROUNDBRACKET = @"\([^)]+\)";
+        public static readonly string RGXCURLYBRACKET = @"\{[^}]*?\}";
+
+        //public static List<IndexableEntity> Parse(Title title, TitleSubtitle titleSubtitle)
+        //{
+        //    string[] theWholeSubtitleFileTextLines = titleSubtitle.SubtitleText.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+        //    List<IndexableEntity> parsedSubtitles = null;
+        //    IndexableEntity currentParsedSubtitle = null;
+        //    TimeSpan startTime = TimeSpan.MinValue;
+        //    TimeSpan endTime = TimeSpan.MinValue;
+        //    bool isPreviousCaptureReadyToInsert = false;
+
+        //    for (int i = 0; i < theWholeSubtitleFileTextLines.Length; i++)
+        //    {
+        //        string thisLine = theWholeSubtitleFileTextLines[i];
+
+        //        int n;
+        //        // Check if this line is not empty and not a number
+        //        if (!string.IsNullOrWhiteSpace(thisLine) && !int.TryParse(thisLine.Trim(), out n))
+        //        {
+        //            if (currentParsedSubtitle == null || currentParsedSubtitle.EndTime != TimeSpan.Zero)
+        //            { currentParsedSubtitle = new IndexableEntity(); }
+
+        //            // This means its a start time and end time indicator
+        //            if (thisLine.Contains("-->"))
+        //            {
+        //                // Capture start time and end time
+        //                string[] timesSplit = thisLine.Split(new string[] { "-->" }, System.StringSplitOptions.RemoveEmptyEntries);
+        //                if (timesSplit.Length == 2)
+        //                {
+        //                    if (!string.IsNullOrWhiteSpace(timesSplit[0]))
+        //                    {
+        //                        string startTimeString = timesSplit[0];
+        //                        startTimeString = startTimeString.Remove(startTimeString.IndexOf(","));
+        //                        startTime = TimeSpan.Parse(startTimeString);
+        //                    }
+
+        //                    if (!string.IsNullOrWhiteSpace(timesSplit[1]))
+        //                    {
+        //                        string endTimeString = timesSplit[1];
+        //                        endTimeString = endTimeString.Remove(endTimeString.IndexOf(","));
+        //                        endTime = TimeSpan.Parse(endTimeString);
+        //                    }
+        //                }
+        //            }
+        //            else // NOW this means its a logical correct text line to capture as a quote
+        //            {
+        //                if (currentParsedSubtitle.StartTime == TimeSpan.Zero) { currentParsedSubtitle.StartTime = startTime; }
+
+        //                thisLine = CleanItOfAllSpecialCharcters(thisLine);
+        //                if (!string.IsNullOrWhiteSpace(currentParsedSubtitle.SubtitleText))
+        //                {
+        //                    currentParsedSubtitle.SubtitleText += " ";
+        //                }
+        //                currentParsedSubtitle.SubtitleText += thisLine;
+
+        //                if ((thisLine.EndsWith(".") || thisLine.EndsWith("?")) && !thisLine.EndsWith("...")) // the last part is just to make sure the statement is not continued
+        //                {
+        //                    isPreviousCaptureReadyToInsert = true;
+
+        //                    currentParsedSubtitle.EndTime = endTime;
+        //                    if (parsedSubtitles == null) { parsedSubtitles = new List<IndexableEntity>(); }
+        //                    currentParsedSubtitle.TitleName = title.TitleName;
+        //                    currentParsedSubtitle.TitleId = title.Id;
+        //                    currentParsedSubtitle.SubtitleTextId = titleSubtitle.Id;
+        //                    parsedSubtitles.Add(currentParsedSubtitle);
+        //                    currentParsedSubtitle = null;
+        //                }
+        //                else
+        //                {
+        //                    isPreviousCaptureReadyToInsert = false;
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    return parsedSubtitles;
+        //}
+
+        //private static string CleanItOfAllSpecialCharcters(string input)
+        //{
+        //    input = RegExReplacer(RGXCURLYBRACKET, input);
+        //    input = RegExReplacer(RGXROUNDBRACKET, input);
+        //    input = RegExReplacer(RGXSQUAREBRACKET, input);
+        //    input = input.Replace("-", string.Empty);
+        //    input = input.Replace("<i>", string.Empty);
+        //    input = input.Replace("</i>", string.Empty);
+        //    input = input.Replace("#", string.Empty);
+        //    input = input.Replace("<", string.Empty);
+        //    input = input.Replace(">", string.Empty);
+        //    input = input.Replace("(", string.Empty);
+        //    input = input.Replace(")", string.Empty);
+        //    return input.Trim();
+        //}
+
+        //private static string RegExReplacer(string regEx, string inputString)
+        //{
+        //    MatchCollection mc = Regex.Matches(inputString, regEx);
+        //    foreach (Match m in mc)
+        //    {
+        //        inputString = inputString.Replace(m.Value, String.Empty);
+        //    }
+        //    return inputString;
+        //}
+
+        public IEnumerable<ParsedText> Parse(MediaText mediaText)
+        {
+            return null;
+        }
+    }
+}
