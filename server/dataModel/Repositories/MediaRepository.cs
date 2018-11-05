6252fd20df1ae5fc237da92efc4e242a8ac77eb6
Pravin Khabile <khabilepravin@gmail.com>
2018-11-05 21:07:22 +1100

    Added AutoFac dependency

--- server/dataModel/Repositories/MediaRepository.cs
+++ server/dataModel/Repositories/MediaRepository.cs
@@ -1,10 +1,17 @@
-﻿namespace dataModel.Repositories
+﻿using System.Threading.Tasks;
+
+namespace dataModel.Repositories
 {
     public class MediaRepository : BaseRepository, IMediaRepository
     {
         public MediaRepository()
         {
 
+        }
+
+        public Task<string> AddMedia()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
