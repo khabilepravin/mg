4e65b73af5c56381681f82b9aff835a0e6285274
Pravin Khabile <khabilepravin@gmail.com>
2018-11-17 22:31:05 +1100

    Search initial test

---
+++ server/test/IndexingTests.cs
@@ -0,0 +1,27 @@
+﻿using dataModel;
+using indexing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace test
+{
+    public class IndexingTests
+    {
+        [Fact]
+        public void TestIndexing()
+        {
+            TextIndexing indexing = new TextIndexing();
+
+            List<ParsedText> itemsToIndex = new List<ParsedText>();
+
+            itemsToIndex.Add(new ParsedText { Text = "Sometimes nothin can be areal cool hand", Id = "10001" });
+            itemsToIndex.Add(new ParsedText { Text = "Not on the rug man", Id = "10002" });
+            itemsToIndex.Add(new ParsedText { Text = "I will make him an offer he can't refuse", Id = "10003" });
+            indexing.IndexText(itemsToIndex);
+        }
+
+       
+    }
+}
