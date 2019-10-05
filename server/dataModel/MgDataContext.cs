6c2fa37ecaebc6267fc73c6fe179d9ab6876a73e
Pravin Khabile <khabilepravin@gmail.com>
2019-10-05 14:45:27 +1000

    Foreign key column indexes

--- server/dataModel/MgDataContext.cs
+++ server/dataModel/MgDataContext.cs
@@ -26,17 +26,17 @@ namespace dataModel
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             #region Comment this region-code for database migration and update
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseMySql("server=localhost;database=mg;user=root;password=p0k5PgOzmgkF");
+            //if (!optionsBuilder.IsConfigured)
+            //{
+            //    optionsBuilder.UseMySql("server=localhost;database=mg;user=root;password=p0k5PgOzmgkF");
 
-                this.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
-            }
+            //    this.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            //}
             #endregion
 
             #region Uncomment this region-code for database migration and update
             //Note: Put the connection string of your db
-            //optionsBuilder.UseMySql("server=localhost;database=mg;user=root;password=p0k5PgOzmgkF");
+            optionsBuilder.UseMySql("server=localhost;database=mg;user=root;password=p0k5PgOzmgkF");
             #endregion
         }
 
@@ -54,18 +54,21 @@ namespace dataModel
             {
                 entity.HasKey(e => e.Id);
                 entity.Property(e => e.MediaId).IsRequired();
+                entity.HasIndex(e => e.MediaId);
             });
 
             modelBuilder.Entity<MediaText>(entity =>
             {
                 entity.HasKey(e => e.Id);
                 entity.Property(e => e.MediaId).IsRequired();
+                entity.HasIndex(e => e.MediaId);
             });
 
             modelBuilder.Entity<ParsedText>(entity =>
             {
                 entity.HasKey(e => e.Id);
                 entity.Property(e => e.MediaTextId).IsRequired();
+                entity.HasIndex(e => e.MediaTextId);
             });
 
             modelBuilder.Entity<TagMaster>(entity =>
@@ -79,6 +82,8 @@ namespace dataModel
                 entity.HasKey(e => e.Id);
                 entity.Property(e => e.MediaId).IsRequired();
                 entity.Property(e => e.TagId).IsRequired();
+                entity.HasIndex(e => e.MediaId);
+                entity.HasIndex(e => e.TagId);
             });
 
             modelBuilder.Entity<TextTag>(entity =>
@@ -86,6 +91,8 @@ namespace dataModel
                 entity.HasKey(e => e.Id);
                 entity.Property(e => e.ParsedTextId).IsRequired();
                 entity.Property(e => e.TagId).IsRequired();
+                entity.HasIndex(e => e.ParsedTextId);
+                entity.HasIndex(e => e.TagId);
             });
 
             modelBuilder.Entity<UserCollection>(entity => 
@@ -93,6 +100,7 @@ namespace dataModel
                 entity.HasKey(e => e.Id);
                 entity.Property(e => e.Name).IsRequired();
                 entity.Property(e => e.UserId).IsRequired();
+                entity.HasIndex(e => e.UserId);
             });
 
             modelBuilder.Entity<UserCollectionItem>(entity =>
@@ -100,6 +108,8 @@ namespace dataModel
                 entity.HasKey(e => e.Id);
                 entity.Property(e => e.CollectionId).IsRequired();
                 entity.Property(e => e.EntityId).IsRequired();
+                entity.HasIndex(e => e.CollectionId);
+                entity.HasIndex(e => e.EntityId);
             });
 
             modelBuilder.Entity<ParsedTextArtist>(entity => 
@@ -107,6 +117,8 @@ namespace dataModel
                 entity.HasKey(e => e.Id);
                 entity.Property(e => e.MediaArtistId).IsRequired();
                 entity.Property(e => e.ParsedTextId).IsRequired();
+                entity.HasIndex(e => e.MediaArtistId);
+                entity.HasIndex(e => e.ParsedTextId);
             });
 
             modelBuilder.Entity<UserFavorite>(entity =>
@@ -114,6 +126,8 @@ namespace dataModel
                 entity.HasKey(e => e.Id);
                 entity.Property(e => e.ParsedTextId).IsRequired();
                 entity.Property(e => e.UserId).IsRequired();
+                entity.HasIndex(e => e.ParsedTextId);
+                entity.HasIndex(e => e.UserId);
             });
         }
     }
---
+++ server/dataModel/Migrations/20191005044139_ForeignIndexes.cs
@@ -0,0 +1,307 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dataModel.Migrations
+{
+    public partial class ForeignIndexes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "UserFavorite",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ParsedTextId",
+                table: "UserFavorite",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EntityId",
+                table: "UserCollectionItem",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CollectionId",
+                table: "UserCollectionItem",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "UserCollection",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TagId",
+                table: "TextTag",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ParsedTextId",
+                table: "TextTag",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ParsedTextId",
+                table: "ParsedTextArtist",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MediaArtistId",
+                table: "ParsedTextArtist",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MediaTextId",
+                table: "ParsedText",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MediaId",
+                table: "MediaText",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TagId",
+                table: "MediaTag",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MediaId",
+                table: "MediaTag",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MediaId",
+                table: "MediaArtist",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFavorite_ParsedTextId",
+                table: "UserFavorite",
+                column: "ParsedTextId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFavorite_UserId",
+                table: "UserFavorite",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCollectionItem_CollectionId",
+                table: "UserCollectionItem",
+                column: "CollectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCollectionItem_EntityId",
+                table: "UserCollectionItem",
+                column: "EntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCollection_UserId",
+                table: "UserCollection",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TextTag_ParsedTextId",
+                table: "TextTag",
+                column: "ParsedTextId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TextTag_TagId",
+                table: "TextTag",
+                column: "TagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParsedTextArtist_MediaArtistId",
+                table: "ParsedTextArtist",
+                column: "MediaArtistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParsedTextArtist_ParsedTextId",
+                table: "ParsedTextArtist",
+                column: "ParsedTextId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParsedText_MediaTextId",
+                table: "ParsedText",
+                column: "MediaTextId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MediaText_MediaId",
+                table: "MediaText",
+                column: "MediaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MediaTag_MediaId",
+                table: "MediaTag",
+                column: "MediaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MediaTag_TagId",
+                table: "MediaTag",
+                column: "TagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MediaArtist_MediaId",
+                table: "MediaArtist",
+                column: "MediaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserFavorite_ParsedTextId",
+                table: "UserFavorite");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserFavorite_UserId",
+                table: "UserFavorite");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserCollectionItem_CollectionId",
+                table: "UserCollectionItem");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserCollectionItem_EntityId",
+                table: "UserCollectionItem");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserCollection_UserId",
+                table: "UserCollection");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TextTag_ParsedTextId",
+                table: "TextTag");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TextTag_TagId",
+                table: "TextTag");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ParsedTextArtist_MediaArtistId",
+                table: "ParsedTextArtist");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ParsedTextArtist_ParsedTextId",
+                table: "ParsedTextArtist");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ParsedText_MediaTextId",
+                table: "ParsedText");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MediaText_MediaId",
+                table: "MediaText");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MediaTag_MediaId",
+                table: "MediaTag");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MediaTag_TagId",
+                table: "MediaTag");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MediaArtist_MediaId",
+                table: "MediaArtist");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "UserFavorite",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ParsedTextId",
+                table: "UserFavorite",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EntityId",
+                table: "UserCollectionItem",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CollectionId",
+                table: "UserCollectionItem",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "UserCollection",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TagId",
+                table: "TextTag",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ParsedTextId",
+                table: "TextTag",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ParsedTextId",
+                table: "ParsedTextArtist",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MediaArtistId",
+                table: "ParsedTextArtist",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MediaTextId",
+                table: "ParsedText",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MediaId",
+                table: "MediaText",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TagId",
+                table: "MediaTag",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MediaId",
+                table: "MediaTag",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MediaId",
+                table: "MediaArtist",
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
