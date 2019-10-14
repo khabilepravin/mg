e31b7152c2553071067a4a0c1853ef7a2c300f81
Pravin Khabile <khabilepravin@gmail.com>
2019-10-14 21:36:09 +1100

    Added serial number property to the ParsedText table

---
+++ server/dataModel/Migrations/20191014100315_AddCol-ParsedText-SerialNumber.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dataModel.Migrations
+{
+    public partial class AddColParsedTextSerialNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SerialNumber",
+                table: "ParsedText",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SerialNumber",
+                table: "ParsedText");
+        }
+    }
+}
--- server/dataModel/ParsedText.cs
+++ server/dataModel/ParsedText.cs
@@ -8,6 +8,7 @@ namespace dataModel
         public string MediaTextId { get; set; }
         public string Text { get; set; }
         public string ArtistId { get; set; }
+        public int SerialNumber { get; set; }
         public TimeSpan StartTime { get; set; }
         public TimeSpan EndTime { get; set; }
         public string GroupId { get; set; }
